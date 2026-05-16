be4313900ad5e3176ca3785d81f279d99de5ae87
reddy0606 <103976992+reddy0606@users.noreply.github.com>
2026-05-16 17:39:12 +0530

    Updated test_cart

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,43 +1,91 @@
+# from pages.login_page import LoginPage
+# from pages.inventory_page import InventoryPage
+# from utils.data_reader import load_test_data
+
+
+# def test_add_item_to_cart(page):
+
+#     data = load_test_data()
+
+#     login_page = LoginPage(page)
+#     inventory_page = InventoryPage(page)
+
+#     login_page.open_login_page()
+
+#     login_page.login(
+#         data["valid_user"]["username"],
+#         data["valid_user"]["password"]
+#     )
+
+#     inventory_page.add_item_to_cart()
+
+#     assert inventory_page.get_cart_count() == "1"
+
+
+# def test_remove_item_from_cart(page):
+
+#     data = load_test_data()
+
+#     login_page = LoginPage(page)
+#     inventory_page = InventoryPage(page)
+
+#     login_page.open_login_page()
+
+#     login_page.login(
+#         data["valid_user"]["username"],
+#         data["valid_user"]["password"]
+#     )
+
+#     inventory_page.add_item_to_cart()
+
+#     inventory_page.remove_item_from_cart()
+
+#     assert page.locator(".shopping_cart_badge").count() == 0
+
+
+
+
+import pytest
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from utils.data_reader import load_test_data
 
 
-def test_add_item_to_cart(page):
-
+@pytest.mark.asyncio
+async def test_add_item_to_cart(page):
     data = load_test_data()
 
     login_page = LoginPage(page)
     inventory_page = InventoryPage(page)
 
-    login_page.open_login_page()
+    await login_page.open_login_page()
 
-    login_page.login(
+    await login_page.login(
         data["valid_user"]["username"],
         data["valid_user"]["password"]
     )
 
-    inventory_page.add_item_to_cart()
+    await inventory_page.add_item_to_cart()
 
-    assert inventory_page.get_cart_count() == "1"
+    assert await inventory_page.get_cart_count() == "1"
 
 
-def test_remove_item_from_cart(page):
-
+@pytest.mark.asyncio
+async def test_remove_item_from_cart(page):
     data = load_test_data()
 
     login_page = LoginPage(page)
     inventory_page = InventoryPage(page)
 
-    login_page.open_login_page()
+    await login_page.open_login_page()
 
-    login_page.login(
+    await login_page.login(
         data["valid_user"]["username"],
         data["valid_user"]["password"]
     )
 
-    inventory_page.add_item_to_cart()
+    await inventory_page.add_item_to_cart()
 
-    inventory_page.remove_item_from_cart()
+    await inventory_page.remove_item_from_cart()
 
-    assert page.locator(".shopping_cart_badge").count() == 0
+    assert (await page.locator(".shopping_cart_badge").count()) == 0
